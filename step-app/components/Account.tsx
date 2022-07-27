77823daba240a190454bfd8ce813f49171c61d02
Edvin Pohto <edvin.pohto@live.fi>
2022-07-27 16:01:32 +0100

    fixed account desktop view

--- step-app/components/Account.tsx
+++ step-app/components/Account.tsx
@@ -5,21 +5,23 @@ export default function Account() {
 
 	return(
 		<div className="bg-white shadow overflow-hidden sm:rounded-lg">
+
 			<div className="px-4 py-5 sm:px-6">
 				<h3 className="text-lg leading-6 font-medium text-gray-900">Account information</h3>
 				<p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and account data.</p>
 			</div>
+
 			<div className="border-t border-gray-200">
 				<dl>
-					<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+					<div className="bg-gray-50 px-4 py-5 sm:px-6">
 						<dt className="text-sm font-medium text-gray-500">Name</dt>
-						<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{session?.user.name}</dd>
+						<dd className="mt-1 text-sm text-gray-900">{session?.user.name}</dd>
 					</div>
-					<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+					<div className="bg-white px-4 py-5 sm:px-6">
 						<dt className="text-sm font-medium text-gray-500">Email address</dt>
-						<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{session?.user.email}</dd>
+						<dd className="mt-1 text-sm text-gray-900">{session?.user.email}</dd>
 					</div>
-					<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+					<div className="bg-gray-50 px-4 py-5 sm:px-6">
 						<button 
 							onClick={() => signOut()}
 							className="text-sm font-medium text-indigo-700">
@@ -27,7 +29,7 @@ export default function Account() {
 						</button>
 					</div>
 					{/* HERE WE NEED TO IMPLEMENT A DELETE ACCOUNT FUNCTION */}
-					<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+					<div className="bg-white px-4 py-5 sm:px-6">
 						<button
 							onClick={() => deleteAccount()}
 							className="text-sm font-medium text-rose-700">
