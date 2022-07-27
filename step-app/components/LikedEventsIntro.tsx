32f588763e7de7d3ea92f353cf68062e7e963c2e
Edvin Pohto <edvin.pohto@live.fi>
2022-07-27 15:38:32 +0100

    changed the liked events page banner

--- step-app/components/LikedEventsIntro.tsx
+++ step-app/components/LikedEventsIntro.tsx
@@ -1,9 +1,10 @@
 export default function Intro() {
   return(
-    <div className="grid place-content-center pt-5">
-      <div className="text-2xl font-bold tracking-wide font-mono">
-        <span className="inline-block italic text-rose-700">Liked</span> events
-      </div>
-    </div>
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+			<div className="px-4 py-5 sm:px-6">
+				<h3 className="text-lg leading-6 font-medium text-gray-900">Liked events</h3>
+				<p className="mt-1 max-w-2xl text-sm text-gray-500">A list of events you have liked and thus saved.</p>
+			</div>
+		</div>
   )
 }
