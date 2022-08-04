c40178d6925a09168ec3dd5d81b5ca26b9e8c367
Edvin Pohto <edvin.pohto@live.fi>
2022-08-04 18:01:41 +0100

    edited the createEditDelete test

--- step-app/cypress/e2e/createEditDelete.cy.ts
+++ step-app/cypress/e2e/createEditDelete.cy.ts
@@ -3,7 +3,8 @@
 describe('Create an event', () => {
   it('Visits the create event page and creates an event', () => {
     cy.visit('/tests/testNewEvent')
-    
+
+    // Create the event
     cy.get('#eventName').type('TestEventId1997')
     cy.get('#eventDate').type('2022-08-05T13:55')
     cy.get('#eventLocation').type('12 Freddie Tait Street, St. Andrews, KY16 8HQ, United Kingdom')
@@ -21,13 +22,14 @@ describe('Create an event', () => {
 
     cy.wait(2000)
 
+    // Finaly check if the new event is included on our events page.
     cy.visit('/tests/testYourEvents')
     cy.contains('TestEventId1997')
   })
 })
 
 describe('Update an event', () => {
-  it('Visits the create event page and creates an event', () => {
+  it('Visits the edit event page and edits an event', () => {
     cy.login();
 		// Visit a route in order to allow cypress to actually set the cookie
 		cy.visit("/");
@@ -55,6 +57,7 @@ describe('Update an event', () => {
 
     cy.wait(2000)
 
+    // Check whether the event has been updated and that the old event is gone
     cy.visit('/tests/testYourEvents')
     cy.contains('EditedTestEventId1997')
   })
@@ -74,6 +77,7 @@ describe('Delete an event', () => {
 
     cy.wait(500)
     
+    // Checks whether the event exists anymore
     cy.contains('EditedTestEventId1997').should('not.exist')
   })
 })
